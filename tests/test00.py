210abe61929db47954fa86d99a7d001f063072d6
Philip Ajuong Deng <philipajuong@gmail.com>
2023-12-24 06:52:33 -0800

    RSA test00

--- tests/test00.py
+++ tests/test00.py
@@ -1,14 +1,16 @@
-4
-12
-34
-128
-1024
-4958
-1718944270642558716715
-9
-99
-999
-9999
-9797973
-49
-239809320265259
+import os
+
+# Ensure the 'tests' directory exists
+os.makedirs("tests", exist_ok=True)
+
+# Specify the full path to the file
+file_path = os.path.join("tests", "test00")
+
+# Open the file in write mode
+with open(file_path, "w") as file:
+    # Write the numbers to the file, one per line
+    numbers = "4\n12\n34\n128\n1024\n4958\n1718944270642558716715\n9\n99\n999\n9999\n9797973\n49\n239809320265259"
+    file.write(numbers)
+
+# Print a confirmation message
+print(f"File '{file_path}' created successfully!")
